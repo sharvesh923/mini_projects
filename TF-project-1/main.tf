11c56c9767c2f0213de083bbb171ae26586fb129
Sharvesh R <113606132+sharvesh923@users.noreply.github.com>
2023-02-10 19:11:45 +0530

    Update main.tf

--- TF-project-1/main.tf
+++ TF-project-1/main.tf
@@ -85,6 +85,10 @@ resource "aws_key_pair" "aws4" {
   key_name   = "aws4"
   public_key = var.public_key_aws4
 }
+# creating data block to ref user_data.tpl
+data "template_file" "user_data" {
+  template = "${file("D:/devops class/terraform/TF-project-1/user_data.tpl")}"
+}
 #creating ubuntu machine based on region selected
 resource "aws_instance" "my_instance" {
   ami           = lookup(var.amis,var.region, "not found")
@@ -93,6 +97,7 @@ resource "aws_instance" "my_instance" {
   subnet_id     = aws_subnet.public_sub_1.id
   vpc_security_group_ids = [aws_security_group.my_sg_1.id]
   associate_public_ip_address = true
+  user_data              = "${data.template_file.user_data.rendered}"
   root_block_device {
     volume_size    = "10"
     volume_type    = "gp2"
